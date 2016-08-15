e7779ee6773f89cfe40dba7a28c97722fe6c3ad1
Adam Breaux <abreaux@kbra.com>
2016-08-15 17:56:42 -0400

    hiera lookups to create some types automagically

--- manifests/init.pp
+++ manifests/init.pp
@@ -31,4 +31,22 @@ class common (
     ensure  => $ensure,
   }
 
+  $crons = hiera_hash('crons',{})
+  create_resources('cron',$crons)
+
+  $files = hiera_hash('files',{})
+  create_resources('file',$files)
+
+  $groups = hiera_hash('groups',{})
+  create_resources('group',$groups)
+
+  $hosts = hiera_hash('hosts',{})
+  create_resources('host',$hosts)
+
+  $keys = hiera_hash('ssh_authorized_keys',{})
+  create_resources('ssh_authorized_key',$keys)
+
+  $users = hiera_hash('users',{})
+  create_resources('user',$users)
+
 }
